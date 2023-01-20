e25f3ff4ea4d714b2838a11ff16cd9f0fb94ea8e
chelseaoktaviany <chelseaoktaviany97@gmail.com>
2023-01-20 16:10:57 +0700

    Added jwt authorization

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,14 +2,28 @@ const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs')
 const asyncHandler = require('express-async-handler');
 const User = require('../model/userModel');
+const {protect} = require('../middleware/authMiddleWare');
 
 //  @desc       Authenticate users
 //  @route      POST /api/users/login
 //  @access     Private
-const loginUser = asyncHandler(async(req, res) => {
-    res.status(200).json({
-        message: 'Login user',
-    })
+const loginUser = asyncHandler(async (req, res) => {
+    const {email, password} = req.body;
+
+    // check for user email
+    const user = await User.findOne({email})
+
+    if(user && (await bcrypt.compare(password, user.password))) {
+        res.json({
+            _id: user.id,
+            name: user.name,
+            email: user.email,
+            token: generateToken(user._id),
+        })
+    } else {
+        res.status(400)
+        throw new Error("Invalid credentials")
+    }
 });
 
 //  @desc       Register new user
@@ -47,6 +61,7 @@ const registerUser = asyncHandler(async (req, res) => {
             _id: user.id,
             name: user.name,
             email: user.email,
+            token: generateToken(user._id),
         })
     } else {
         res.status(400)
@@ -58,10 +73,22 @@ const registerUser = asyncHandler(async (req, res) => {
 //  @route      POST /api/users/me
 //  @access     Private
 const getMe = asyncHandler(async (req, res) => {
+    const {_id, name, email} = await User.findById(req.user.id)
+
     res.status(200).json({
-        message: 'User data display',
+        id: _id,
+        name,
+        email,
     })
 });
+
+// generate JWT
+const generateToken = (id) => {
+    return jwt.sign({id}, process.env.JWT_SECRET, {
+        expiresIn: '30d',
+    })
+}
+
 
 module.exports = {
     loginUser,
