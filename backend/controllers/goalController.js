5d4fb07341bee373b2995846ddd66cf2e76eefc9
chelseaoktaviany <chelseaoktaviany97@gmail.com>
2023-01-20 16:41:39 +0700

    modified controller for delete goals

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -70,7 +70,19 @@ const deleteGoal = asyncHandler(async (req, res) => {
         throw new Error('Goal not found!')
     }
 
-    
+    const user = await User.findById(req.user.id)
+
+    // check for user
+    if(!user) {
+        res.status(401)
+        throw new Error('User not found')
+    }
+
+    // make sure the logged in user matches the goal user
+    if(goal.user.toString() !== user.id) {
+        res.status(401)
+        throw new Error('User not authorized')
+    }
 
     await goal.remove()
 
