5fb173241baa6593dd65e8a54ba3441a0c2d9708
chelseaoktaviany <chelseaoktaviany97@gmail.com>
2023-01-20 16:15:55 +0700

    modified goal routes and controller

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -9,14 +9,11 @@ const {
     deleteGoal
 } = require('../controllers/goalController');
 
+const {protect} = require('../middleware/authMiddleWare');
+
 // for clean code
-router.route('/').get(getGoals).post(setGoal);
-router.route('/:id').put(updateGoal).delete(deleteGoal);
+router.route('/').get(protect, getGoals).post(protect, setGoal);
+router.route('/:id').put(protect, updateGoal).delete(protect, deleteGoal);
 
-
-// router.get('/', getGoals);
-// router.post('/', setGoal);
-// router.put('/:id', updateGoal);
-// router.delete('/:id', deleteGoal);
 
 module.exports = router;
